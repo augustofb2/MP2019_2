1a1ebe23157ea4e8b2124742c060f660f89e8cac
Plinio Mayer <pctmayer@gmail.com>
2019-11-22 17:39:22 -0300

    json das disciplinas

--- js/disciplina/disciplina.js
+++ js/disciplina/disciplina.js
@@ -1,11 +1,14 @@
 class disciplina {
   constructor(inicial) {
+  	this.nome = inicial.nome;
     this.creditos = inicial.creditos;
     this.provas = inicial.provas;
     this.trabalhos = inicial.trabalhos;
     this.estudo = inicial.estudo;
     this.nota = inicial.nota;
     this.desempenho = inicial.desempenho;
+    this.reprovado = inicial.reprovado;
+    this.semestre = inicial.semestre;
   }
 }
 
