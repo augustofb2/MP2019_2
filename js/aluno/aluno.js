e3a4f1918ebd778d15d6e15f5cc75f0054425ef9
Augusto Freitas <augustofb2@gmail.com>
2019-11-22 16:35:11 -0300

    Esqueleto de classes

---
+++ js/aluno/aluno.js
@@ -0,0 +1,17 @@
+class aluno {
+  constructor(inicial) {
+    this.nome = inicial.nome;
+    this.semestre = inicial.semestre;
+    this.disciplinas = inicial.disciplinas;
+    this.dinheiro = inicial.dinheiro;
+    this.sono = inicial.sono;
+    this.lazer = inicial.lazer;
+    this.estresse = inicial.estresse;
+    this.cansaço = inicial.cansaço;
+    this.estagiando = inicial.estagiando;
+    this.experiencia = inicial.experiencia;
+  }
+}
+
+
+export default aluno;
---
+++ js/disciplina/disciplina.js
@@ -0,0 +1,12 @@
+class disciplina {
+  constructor(inicial) {
+    this.creditos = inicial.creditos;
+    this.provas = inicial.provas;
+    this.trabalhos = inicial.trabalhos;
+    this.estudo = inicial.estudo;
+    this.nota = inicial.nota;
+    this.desempenho = inicial.desempenho;
+  }
+}
+
+export default disciplina;
